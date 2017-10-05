4272ce8dd6871f2fe19afb098c127b65b7283281
Daniel Kaschel <dipique@gmail.com>
2017-10-05 14:55:02 -0500

    Set up most of the js and logic for the hero picking screen. The detail loading function still doesn't work, and when it does the layout will need to be totally reworked.

--- DotAPicker/Controllers/HomeController.cs
+++ DotAPicker/Controllers/HomeController.cs
@@ -4,27 +4,65 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using DotAPicker.Models;
+
 namespace DotAPicker.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : DotAController
     {
+        // GET: Hero
         public ActionResult Index()
         {
-            return View();
+            ViewBag.SelectedHeroID = TempData["SelectedHeroID"];
+            return View("DotAPicker", db.Heroes.OrderBy(h => h.Name));
         }
 
-        public ActionResult About()
+        // GET: Hero/Create
+        public ActionResult Create()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
+            var hero = new Hero();
+            if (db.Heroes.Count() > 0) hero.ID = db.Heroes.Max(h => h.ID) + 1;
+            return View("Create", hero);
         }
 
-        public ActionResult Contact()
+        // POST: Hero/Create
+        [HttpPost]
+        public ActionResult Create(Hero model)
         {
-            ViewBag.Message = "Your contact page.";
+            if (db.Heroes.Any(h => h.Name == model.Name))
+            {
+                throw new Exception("This name is already in use");
+            }
 
-            return View();
+            if (db.Heroes.Any(h => h.ID == model.ID))
+            {
+                throw new Exception("Hero ID collision");
+            }
+
+            db.Heroes.Add(model);
+            db.Save();
+            return Index();
+        }
+
+        public ActionResult Detail(int id)
+        {
+            return PartialView(new HeroDetailViewModel(id, db));
+        }
+
+        // GET: Hero/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var hero = db.Heroes.FirstOrDefault(h => h.ID == id);
+            if (hero == null)
+            {
+                throw new Exception("Hero not found.");
+            }
+
+            db.Heroes.Remove(hero);
+
+            db.Save();
+            db = DotADB.Load();
+            return Index();
         }
     }
 }
