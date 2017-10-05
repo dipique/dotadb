e8484d05d0c807566d9f37e7394dfc2959ec2896
Daniel Kaschel <dipique@gmail.com>
2017-10-05 09:27:36 -0500

    Add settings page with ability to edit them

---
+++ DotAPicker/Controllers/SettingsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using DotAPicker.Models;
+
+namespace DotAPicker.Controllers
+{
+    public class SettingsController : DotAController
+    {
+        // GET: Settings
+        public ActionResult Index()
+        {
+            return View("Edit", db.Settings);
+        }
+
+        public ActionResult Save(DotASettings model)
+        {
+            //Repace the Tip with the edited Tip
+            db.Settings = model;
+            db.Save();
+            db = DotADB.Load();
+            return Index();
+        }
+    }
+}
