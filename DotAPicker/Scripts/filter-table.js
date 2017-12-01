50dd7cccb823437cb1cae7a0a37ff92f8bb240ec
Daniel Kaschel <dipique@gmail.com>
2017-12-01 14:55:47 -0600

    Changed tip and relationship page to divs (from table), plus unified the filterTable design and made it much more adaptible via a little configuration object on each page

--- DotAPicker/Scripts/filter-table.js
+++ DotAPicker/Scripts/filter-table.js
@@ -1,13 +1,42 @@
-﻿//Derived from: https://www.w3schools.com/howto/howto_js_filter_table.asp
-//This is what enables filtering the hero list by name and preference
-function filterFunction() {
-    // Get search criteria
-    var input = document.getElementById("filterInput");
-    var filter = input.value.toUpperCase();
-    var table = document.getElementById("filterHeroTable");
+﻿//a filter set is a configuration object required for each filterTable. It must be a variable named "filterSet" accessible by filterFunction()
+var sampleFilterSet = [[
+        "nameFilter",  //id of object with filter value
+        "multi",       //type of element with filter value (multi: multi-select, single: single-select, text: input box, rowIndexExclusion)
+        "nameValue",   //id of header cells that contain the value
+        "inner"        //method of retrieving cell value (inner: inner HTML, select: select box option)
+    ], [
+        "preferenceFilter",
+        "single",
+        "preferenceValue",
+        "select"
+    ], [
+        "typeFilter",
+        "single",
+        "typeValue",
+        "inner"
+    ], [
+        "textFilter",
+        "text",
+        "textValue",
+        "inner"
+    ], [
+        "rowIdFilter",
+        "row-index-exclusion", //this is a special kind of filter that excludes items based on an array of row IDs
+        "row",
+        "array"
+    ]
+];
 
-    //get search preference
-    var searchPreference = getPipedPreferenceSelections();
+
+//Hide the table rows that do not meet the criteria
+function filterFunction() {
+    //get table
+    var table = document.getElementById("filterTable");
+
+    //get filters
+    var filters = getSearchCriteriaFromFilterSet(filterSet);
+    var rowFilter = filters.get("row");
+    var hasRowFilter = typeof rowFilter != "undefined";
 
     //loop through the table rows
     for (var i = 0; i < table.childNodes.length; i++) {
@@ -19,40 +48,62 @@ function filterFunction() {
             continue;
         }
 
-        //get the cell contents
-        var heroName, heroAltNames, heroPreference;
-        for (var j = 0; j < row.childNodes.length; j++) {
-            //only process the loop for cells
-            var cell = row.childNodes[j];
-            if (typeof cell.className == "undefined") {
-                continue;
-            } else if (cell.className.indexOf("data-row-cell") == -1) {
-                continue;
-            }
-
-            //get the cell contents
-            switch (cell.id) {
-                case "heroName":
-                    heroName = cell.innerHTML.toUpperCase();
-                    break;
-                case "heroAltNames":
-                    heroAltNames = cell.innerHTML.toUpperCase();
-                    break;
-                case "heroPreference":
-                    var selBox = $(cell).find("select")[0];
-                    heroPreference = selBox.options[selBox.selectedIndex].text;
-                    break;
+        //check for a row exclusions before checking all the cell values
+        var hideRow = false;
+        var rowId = row.id;
+        if (hasRowFilter && rowId) { //if there's a row ID filter and the row has an ID...
+            if (rowFilter.searchValue.includes("|" + rowId + "|")) {
+                hideRow = true;
             }
         }
 
-        //determine whether to hide the row
-        var hideRow = false;
-        if (searchPreference != "" && !searchPreference.includes(heroPreference)) { //look for matching preference
-            hideRow = true;
-        } else if (filter != "" && !(heroName.includes(filter) || heroAltNames.includes(filter))) { //look for matching filter
-            hideRow = true;
-        } else if (typeof allyArray !== 'undefined' && typeof enemyArray !== 'undefined') { //if there is an enemy/ally array in scope
-            hideRow = allyArray.indexOf(row.id) > -1 || enemyArray.indexOf(row.id) > -1;    //check if the hero is already assigned, and hide if so
+        //now check the cell values
+        if (!hideRow) {
+            var cellID, cellValue, filter;
+            for (var j = 0; j < row.childNodes.length; j++) {
+                //only process the loop for cells
+                var cell = row.childNodes[j];
+                if (typeof cell.className == "undefined") {
+                    continue;
+                } else if (cell.className.indexOf("data-row-cell") == -1) {
+                    continue;
+                }
+
+                //check if there is a filter on this cell
+                filter = filters.get(cell.id);
+                if (typeof filter == "undefined") { //if there's no filter on this cell
+                    continue;
+                }
+
+                //get the cell contents
+                var cellContents;
+                switch (filter.fetchType) {
+                    case "inner":
+                        cellContents = cell.innerHTML.toUpperCase().trim();
+                        break;
+                    case "select":
+                        var selBox = $(cell).find("select")[0];
+                        cellContents = selBox.options[selBox.selectedIndex].text.toUpperCase().trim();
+                        break;
+                }
+
+                //now see if it matches the filter
+                switch (filter.matchType) {
+                    case "partial":
+                        hideRow = !cellContents.includes(filter.searchValue);
+                        break;
+                    case "partial-reverse":
+                        hideRow = !filter.searchValue.includes(cellContents);
+                        break;
+                    case "full":
+                        hideRow = !cellContents == filter.searchValue;
+                        break;
+                }
+
+                if (hideRow) { //don't keep looking if we've found exclusion criteria
+                    break;
+                }
+            }
         }
 
         //hide the row, if necessary
@@ -67,21 +118,72 @@ function filterFunction() {
     showNoDataElements();
 }
 
-function getPipedPreferenceSelections() {
-    var selBox = $("#filterPreference")[0];
+//processes the filterSet into criteria that can be applied to the filter table
+function getSearchCriteriaFromFilterSet() {
+    var returnSet = new Map();
+    filterSet.forEach(function (filter) {
+        var element = document.getElementById(filter[0]);
+        var filterType = filter[1];
+        if (!element && filterType != "row-index-exclusion") { //if filter doesn't exist, exit
+            return;                                            //except row exclusions, which 
+        }                                                      //aren't mapped to elements
+        var searchValue, matchType;
+        switch (filterType) {
+            case "single":
+                searchValue = element.options[element.selectedIndex].text.toUpperCase();
+                matchType = "partial";         //entire search term is contained within cell contents
+                break;
+            case "multi":
+                searchValue = getPipedMultiSelectOptions(element);
+                matchType = "partial-reverse";  //entire cell contents is contains in search term
+                break;
+            case "text":
+                searchValue = element.value.toUpperCase();
+                matchType = "partial";          //entire search term is equal to cell contents
+                break;
+            case "row-index-exclusion":
+                searchValue = arrayToPipedOptions(window[filter[0]]); //get the array of index values
+                matchType = "row-exclusion";    //row index (|ind|) contained in search term
+                break;
+        }
+        var valueCellID = filter[2];
+        if (searchValue.length < 1) { //if there's no search filter here, don't add it to the map
+            return;
+        }
+
+        returnSet.set(valueCellID, {
+            searchValue: searchValue,
+            matchType: matchType,
+            fetchType: filter[3]
+        });
+    });
+
+    return returnSet;
+}
+
+//gets all the options selected in a multi-select box and returns them as a pipe-delimited string
+function getPipedMultiSelectOptions(selBox) {
     var searchPreference = "";
     for (var x = 0; x < selBox.options.length; x++) {
         if (selBox.options[x].selected && selBox.options[x].value != "") {
-            searchPreference += selBox.options[x].value + "|"; //the pipe just eliminates partial matches
+            searchPreference += selBox.options[x].value + "|"; //the pipe eliminates (most) partial matches
         }
     }
 
-    return searchPreference;
+    return searchPreference.toUpperCase();
 }
 
-function countVisibleHeroes() {
+function arrayToPipedOptions(array) {
+    var retVal = "|";  //this seed is what guarantees partial match elimination
+    for (var x = 0; x < array.length; x++) {
+        retVal += array[x] + "|";                //the pipe eliminates all partial matches
+    }
+    return retVal.toUpperCase();
+}
+
+function countVisibleItems() {
     // Get search criteria
-    var table = document.getElementById("filterHeroTable");
+    var table = document.getElementById("filterTable");
     var visibleCount = 0;
 
     //loop through the table rows
@@ -109,16 +211,17 @@ function showNoDataElements() {
         return;
     }
 
-    if (countVisibleHeroes() == 0) {
+    if (countVisibleItems() == 0) {
         noData[0].style.display = "";
     } else {
         noData[0].style.display = "none";
     }
 }
 
-function firstVisibleHeroIndex() {
+//Gets the if of the first visible row
+function firstVisibleItemIndex() {
     // Get search criteria
-    var table = document.getElementById("filterHeroTable");
+    var table = document.getElementById("filterTable");
 
     //loop through the table rows
     for (var i = 0; i < table.childNodes.length; i++) {
