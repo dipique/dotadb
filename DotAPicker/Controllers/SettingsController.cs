2d780ce4b54ad5bff5eba6721d8cb557e64e0a9d
Daniel Kaschel <dipique@gmail.com>
2017-12-07 09:27:52 -0600

    Created user action scaffolding (no functionality yet)

---
+++ DotAPicker/Controllers/SettingsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using DotAPicker.DAL;
+using DotAPicker.Models;
+using DotAPicker.Utilities;
+using DotAPicker.ViewModels;
+
+namespace DotAPicker.Controllers
+{
+    public class SettingsController: DotAController
+    {
+        public ActionResult UserSettings() => View();
+    }
+}
---
+++ DotAPicker/ViewModels/UserActionsViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace DotAPicker.ViewModels
+{
+    public class PasswordResetViewModel
+    {
+        [Required]
+        [RegularExpression(@"^(?=.*[a-zA-Z\d].*)[a-zA-Z\d!@#$%&*]{3,}$")]
+        [Display(Name = "Current Password")]
+        public string CurrentPassword { get; set; }
+
+        [Required]
+        [RegularExpression(@"^(?=.*[a-zA-Z\d].*)[a-zA-Z\d!@#$%&*]{3,}$")]
+        [Display(Name = "New Password")]
+        public string NewPassword { get; set; }
+
+        [Required]
+        [RegularExpression(@"^(?=.*[a-zA-Z\d].*)[a-zA-Z\d!@#$%&*]{3,}$")]
+        [Compare(nameof(NewPassword))]
+        [Display(Name = "Re-Enter Password")]
+        public string ComparePassword { get; set; }
+    }
+
+    public class DeleteProfileViewModel
+    {
+        [Required]
+        [RegularExpression(@"^(?=.*[a-zA-Z\d].*)[a-zA-Z\d!@#$%&*]{3,}$")]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+    }
+
+    public class ReplaceProfileViewModel
+    {
+        [Required]
+        [RegularExpression(@"^(?=.*[a-zA-Z\d].*)[a-zA-Z\d!@#$%&*]{3,}$")]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Required]
+        [Display(Name = "Profile To Copy")]
+        public string ProfileToCopy { get; set; }
+    }
+}
