af9ef2560bcbb5b83a71acc9376fe90ca26ba0dd
Daniel Kaschel <dipique@gmail.com>
2017-12-27 15:10:42 -0600

    Added ability to import labels

--- DotAPicker/Controllers/SettingsController.cs
+++ DotAPicker/Controllers/SettingsController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Web.Mvc;
@@ -118,7 +119,6 @@ namespace DotAPicker.Controllers
                 throw;
                 //return View(new ImportProfileViewModel()).Error("Failed while trying to import profile data.");
             }
-
         }
 
         [HttpGet]
@@ -143,6 +143,70 @@ namespace DotAPicker.Controllers
             }
 
             return View(new ProfileSettingsViewModel() { ProfileType = CurrentUser.ProfileType }).Success("Profile type changed!");
+        }
+
+        [HttpGet]
+        public ActionResult ImportLabels() => View(new ImportLabelsViewModel());
+        public ActionResult ImportLabels(ImportLabelsViewModel viewModel)
+        {
+            if (viewModel.PostedFile == null) return View(viewModel).Error("Umm... you know you DO need to upload SOMETHING, don't you?");
+
+            int MAX_LABEL_FILE_SIZE = 100000; //no more than 100KB, which is already a shit ton of labels
+            char FILE_DELIM_CHAR = '\t';
+            char[] controlChars = { '\t', '\r', '\n' };
+            char[] allowedExtraChars = { ' ', '-', '\''};
+            if (viewModel.PostedFile.ContentLength > MAX_LABEL_FILE_SIZE) return View(viewModel).Error("Seriously, how many labels you got?! Fuck off. (Or you might have picked the wrong file or tried to upload an Excel file or some shit.");
+
+            //get the string that represents the file contents
+            var contents = new StreamReader(viewModel.PostedFile.InputStream).ReadToEnd().Replace("\r", "");
+
+            //check for weird shit
+            if (contents.Any(c => !char.IsLetterOrDigit(c) && !controlChars.Contains(c) && !allowedExtraChars.Contains(c))) return View(viewModel).Error("Let's stick with letters and numbers in our damn labels iffen you please.");
+            var lines = contents.Split('\n').Where(s => !string.IsNullOrWhiteSpace(s)).ToArray();
+            if (lines.Count() <= 1) return View(viewModel).Error("Gotta give me more than that buddy. At least one hero per label.");
+
+            //get the labels
+            var labels = lines[0].Split(FILE_DELIM_CHAR).Select(s => s.Trim()).ToArray();
+            var labelCount = labels.Count();
+
+            //create the list to be imported
+            var labelAssignments = new List<KeyValuePair<string, string>>(); //hero name, label name
+            for (int lineIndex = 1; lineIndex < lines.Count(); lineIndex++)
+            {
+                var heroes = lines[lineIndex].Split(FILE_DELIM_CHAR).Select(s => s.Trim())
+                                                                    .Select(s => s.Length == 1 ? string.Empty : s) //removes single-character labels
+                                                                    .ToArray();
+                if (heroes.Count() > labelCount) return View(viewModel).Error("Seems like there are more hero categories than actual labels, which is a problem.");
+                for (int heroIndex = 0; heroIndex < heroes.Count(); heroIndex++)
+                {
+                    var label = labels[heroIndex];
+                    if (!string.IsNullOrWhiteSpace(label))
+                    {
+                        labelAssignments.Add(new KeyValuePair<string, string>(heroes[heroIndex], labels[heroIndex]));
+                    }
+                }
+            }
+
+            //We've made it to this point, let's add some labels!
+            foreach (var heroLabels in labelAssignments.GroupBy(la => la.Key)
+                                                       .Where(lag => CurrentUser.Heroes.Any(h => h.Name.ToUpper() == lag.Key.ToUpper()))) //where the hero exists already
+            {
+                var hero = CurrentUser.Heroes.First(h => h.Name.ToUpper() == heroLabels.Key.ToUpper());
+                var newLabels = heroLabels.Select(kvp => kvp.Value) //get the labels for the hero
+                                          .Where(lbl => !hero.DescriptionLabels.Any(dl => dl.ToUpper().Trim() == lbl.ToUpper())); //but only those that don't already exist
+                if (newLabels.Count() == 0) continue; //don't make any changes if none of the labels are new
+
+                //add the new labels
+                hero.DescriptionLabels.AddRange(heroLabels.Select(kvp => kvp.Value));
+                hero.DescriptionLabels.RemoveAll(l => string.IsNullOrWhiteSpace(l)); //remove any empty labels
+                db.Entry(hero).State = EntityState.Modified;
+                if (!db.SaveChangesB(true))
+                {
+                    return View(viewModel).Error("Sorry, there was a problem saving the changes to the database. Maybe you're too ugly.");
+                }
+            }
+
+            return View().Success("Labels imported successfully! Frankly I'm a little surprised.");
         }
 
     }
---
+++ DotAPicker/ViewModels/ImportLabelsViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace DotAPicker.ViewModels
+{
+    public class ImportLabelsViewModel
+    {
+        [DataType(DataType.Upload)]
+        [Display(Name = "File to Upload")]
+        public HttpPostedFileBase PostedFile { get; set; }
+    }
+
+}
