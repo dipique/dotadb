9fcb3331558a55671d478aad0e29ca9408b0ba53
Daniel Kaschel <dipique@gmail.com>
2018-01-05 16:49:09 -0600

    Created email object so we can start sending password reset e-mails

---
+++ DotAPicker/Models/Utilities/Email.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+
+namespace DotAPicker.Models.Utilities
+{
+    public class Email
+    {
+        public static bool SendEmail(string to, string subject, string body)
+        {
+            MailMessage msg = new MailMessage("daniel@dotapad.com", to);
+            msg.Subject = subject;
+            msg.Body = body;
+            msg.IsBodyHtml = false;
+
+            var client = new SmtpClient("smtp.zoho.com", 465);
+            client.UseDefaultCredentials = false;
+            client.Credentials = new NetworkCredential("daniel@dotapad.com", "Dizz0ap4d^^");
+            client.EnableSsl = true;
+
+            try
+            {
+                client.Send(msg);
+                return true;
+            }
+            catch (Exception) { return false; }
+        }
+    }
+}
