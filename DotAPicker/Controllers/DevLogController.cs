3ea720b59e8f5cb61e8069a9d5d842930c16dd81
Daniel Kaschel <dipique@gmail.com>
2018-03-28 12:14:03 -0500

    Updated devlog, confirmed functional now

---
+++ DotAPicker/Controllers/DevLogController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DotAPicker.Controllers
+{
+    public class DevLogController : Controller
+    {
+        // GET: DevLog
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
